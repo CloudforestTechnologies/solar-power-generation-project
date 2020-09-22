98ee5657338b85b5f06423b2ea6a8df17fa82dd9
PMetcalf <paulmetcalf@hotmail.com>
2020-09-22 10:06:29 +0100

    Retrieve dataset method outline.

--- Source/data/kaggle_api_helper.py
+++ Source/data/kaggle_api_helper.py
@@ -3,3 +3,21 @@ kaggle api helper
 
 Providers helper methods for downloading and working with kaggle datasets via api. 
 '''
+
+# Module importations (A - Z)
+
+# Constants
+
+def retrieve_kaggle_dataset():
+    """Retrieve Kaggle Dataset
+    ======================================
+    Returns a dataset from kaggle using specified url.
+    
+    Args:
+        dataset_url (str) - Url for dataset.
+        is_zip (bool) - Indicates whether dataset is returned as zip (will unzip).
+        
+    Returns:
+        dataset (var) - Dataset downloaded from kaggle site.
+    """
+    pass
