55fc2435cb48f0a1f23c3e7bb016662eeef6152f
PMetcalf <paulmetcalf@hotmail.com>
2020-10-14 16:42:20 +0100

    Figures helper import.

---
+++ Source/visualisation/figures_helper.py
@@ -0,0 +1,38 @@
+'''
+figures_helper
+
+This file supports operations on figures, such as saving files.
+'''
+
+# Module Importations
+from datetime import datetime
+
+TITLE_FONTSIZE = 18
+PROJECT_CODE = 'WJ'
+SAVE_FORMAT = '.png'
+
+# Helper Method for saving figures
+def generate_fig_save_string(filename):
+    """Save Figures
+    ======================================
+    Saves figures using prescribed filename, date/timestamp and directory.
+    
+    Args:
+        filename (string) - Filename to be used for figure.
+        
+    Returns:
+        save_string (string) - String to use for saving file.
+    """
+
+    # Save files to project
+    filedirectory = r'C:/Developer/solar-power-generation-project/Reports/Figures'
+
+    # Retrieve timestamp
+    timestamp = datetime.now()
+    timestamp_str = timestamp.strftime('%Y_%m_%d-%H_%M_%S')
+
+    # Create filepath
+    filepath = filedirectory + '/' + PROJECT_CODE + '_' + filename + '_' + timestamp_str + SAVE_FORMAT
+
+    # Return save string
+    return filepath
