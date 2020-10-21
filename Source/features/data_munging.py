37e5481163cc5da75d7786078e1d9deb35b82b90
PMetcalf <paulmetcalf@hotmail.com>
2020-10-21 15:44:25 +0100

    wip.

--- Source/features/data_munging.py
+++ Source/features/data_munging.py
@@ -35,7 +35,22 @@ def return_cell_number(source_key):
         source_key (str) - Source key for the .
         
     Returns:
-        cell_number (int) - Cell number as an integer.
+        cell_number (str) - Cell number as a string.
     """
 
-    pass
+    # Source Key - Cell Number dictionary
+    cell_number_dict = {
+        '1BY6WEcLGh8j5v7' : '1', '1IF53ai7Xc0U56Y' : '2', '3PZuoBAID5Wc2HD' : '3', '7JYdWkrLSPkdwr4' : '3',
+        'McdE0feGgRqW7Ca' : '5', 'VHMLBKoKgIrUVDU' : '6', 'WRmjgnKYAwPKWDb' : '7', 'ZnxXDlPa8U1GXgE' : '8',
+        'ZoEaEvLYb1n2sOq' : '9', 'adLQvlD726eNBSB' : '10', 'bvBOhCH3iADSZry' : '11', 'iCRJl6heRkivqQ3' : '12',
+        'ih0vzX44oOqAx2f' : '13', 'pkci93gMrogZuBj' : '14', 'rGa61gmuvPhdLxV' : '15', 'sjndEbLyjtCKgGv' : '16',
+        'uHbuxQJl8lW7ozc' : '17', 'wCURE6d3bPkepu2' : '18', 'z9Y9gH1T5YWrNuG' : '19', 'zBIq5rxdHJRwDNY' : '20',
+        }
+
+    # Try to find cell number in dict
+    try:
+        cell_number = cell_number_dict(source_key)
+    except:
+        cell_number = ""
+
+    # Return cell number
