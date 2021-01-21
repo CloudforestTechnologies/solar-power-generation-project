66eacee14a8c802236731915dc2cf6f7c163f172
PMetcalf <paulmetcalf@hotmail.com>
2021-01-21 09:52:22 +0000

    Work in progress.

--- Source/constants.py
+++ Source/constants.py
@@ -4,9 +4,17 @@ Constants
 Provides store of constants for use with this project
 
 '''
+# Module Imports
+import os
 
 def return_project_code():
     return 'WJ'
 
 def return_model_save_path():
     return r'C:\Developer\solar-power-generation-project\Models'
+
+def return_root_dir():
+    
+    print(os.curdir)
+    
+    return os.curdir
--- Source/models/tensorboard_helpers.py
+++ Source/models/tensorboard_helpers.py
@@ -6,3 +6,7 @@ Helper routines for visualising model training performance using TensorBoard.
 '''
 
 # Module Importations
+import os
+import time
+
+os.curdir
